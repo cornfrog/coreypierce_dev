1adc83d97a79f4bf9deec63d73754df2409f3515
Corn <cornfrog@gmail.com>
2026-05-07 22:33:46 -0400

    testing something

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,13 @@ import emailjs from "@emailjs/browser";
 import Page from "../components/Page";
 
 const Contact = () => {
+
+    console.log("ENV CHECK:", {
+        service: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        template: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        key: import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+    });
+
     const [form, setForm] = useState({
         email: "",
         subject: "",
